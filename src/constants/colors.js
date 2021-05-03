a87155c248bbd24acb08ee8a26952ef83eb3cd3f
Fayra Miranda <miranda.fayra@gmail.com>
2021-05-03 11:10:11 -0300

    Fazer base organização

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,3 +1,3 @@
-export const primaryColor = #e86e5a 
-export const baseColor = #ffffff 
-export const secondaryColor = #b8b8b8
+export const primaryColor = '#e86e5a' 
+export const baseColor = '#ffffff' 
+export const secondaryColor = '#b8b8b8'
