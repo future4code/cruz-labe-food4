61c49560f526fce0c460734f6ab622af02f6bf3e
Lua-Monteiro <78241482+Lua-Monteiro@users.noreply.github.com>
2021-05-04 01:40:51 -0300

    Feat - AddressPage

---
+++ src/pages/AdressPage/AdressPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdressForm from './AdressForm'
+import { ScreenContainer } from './StyledAdress'
+
+const AdressPage = () => {
+
+  return (
+    <ScreenContainer>
+      <AdressForm />
+    </ScreenContainer>
+  )
+}
+
+export default AdressPage
