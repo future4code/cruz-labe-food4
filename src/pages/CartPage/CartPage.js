34ed14f3fbf2a95f803474f7e26ff873be2ccf00
nathaliacardoso <nathalia.mscardoso@gmail.com>
2021-05-05 20:28:47 -0300

    Page Cart add

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import {DivCart, HederText, ButtonConfirm, DivUniqueProduct, DivProductImage, DivFrete, DivTotalPrice, DivPayment} from './StyledCart'
+// import EditAdress from '../../../components/EditAdressCard/EditAdressCard'
+// import ProfileInfos from '../../../components/ProfileInfos/ProfileInfos'
 
 const CartPage = () =>{
     return (
-    <div>
-       <h1>CartPage</h1> 
-    </div>
+        <DivCart>
+            <HederText>Meu Carrinho</HederText> 
+            <hr />
+            <div> Endereço de Entrega </div>
+            <div> Infos de Entrega </div>
+            <div> 
+                <DivUniqueProduct>
+                    <DivProductImage>
+                    </DivProductImage>    
+                    Produto 1    
+                </DivUniqueProduct>  
+                <DivUniqueProduct> 
+                <DivProductImage>
+                    </DivProductImage>
+                    Produto 2
+                </DivUniqueProduct>   
+            </div>
+            <DivFrete>Frete R$6,00</DivFrete>
+            <DivTotalPrice>SUBTOTAL </DivTotalPrice>
+            <DivPayment> Forma de Pagamento </DivPayment>
+            <hr />
+            {/* <div>
+            <form>
+            <input type="radio" name="dinheiro" value="op1"> Dinheiro
+            <input type="radio" name="credito" value="op2"> Cartão de Crédito
+            </form>
+            </div> */}
+            <ButtonConfirm> Confirmar </ButtonConfirm>
+            {/* <ProfileInfos/> */}
+            {/* <EditAdress/> */}
+        </DivCart>
     )
 } 
 
 export default CartPage
+
---
+++ src/pages/CartPage/StyledCart.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components'
+
+export const DivCart = styled.div `
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+`
+
+export const HederText = styled.p `
+    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, 
+    Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size:16px;
+    font-weight:500;
+    padding-top:12px;
+`
+
+export const ButtonConfirm = styled.button`
+  /* width: 328px; */
+  width: 25.68vw;
+  /* height: 42px; */
+  height: 6.017vh;
+  /* padding: 12px 16px; */
+  padding: 1.719vh 2.292vw;
+  border-radius: 2px;
+  background-color: #e86e5a;
+  color: var(--dark-peach);
+  /* font-family: Roboto; */
+  /* font-size: 16px; */
+  font-size: 2.29vh;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+`
+
+export const DivUniqueProduct = styled.div`
+  /* width: 328px; */
+  width: 25.68vw;
+  /* height: 112px; */
+  height: 16.04vh;
+  margin: 1px 0 0;
+  padding: 0 0 0 113px;
+  border-radius: 8px;
+  border: solid 1px #b8b8b8;  
+`
+
+export const DivProductImage = styled.img`
+  height: 16.13vh;
+  width: 7.59vw;
+  background-color: #d8d8d8;
+  border-radius: 8px 0 0 8px;
+`
+
+export const DivFrete = styled.div`
+  width: 25.68vw;
+  text-align: right;
+  font-size: 2.29vh;
+  color: #000;
+`
+
+export const DivTotalPrice = styled.div`
+  width: 25.68vw;
+  font-size: 2.29vh;
+  color: #000;
+`
+
+export const DivPayment = styled.div`
+  width: 25.68vw;
+  font-size: 2.29vh;
+  
+`
