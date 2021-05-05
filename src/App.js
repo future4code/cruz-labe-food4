7f3863370286511ca98b70aa159656d647923ce3
Amanda Lentez <amanda.a.lentz@gmail.com>
2021-05-05 18:04:11 -0300

    Footer + RestaurantCard (feedpage)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
+import { Footer } from 'antd/lib/layout/layout';
 import React from 'react'
 import Router from './routes/router'
+import Foot from './components/Footer/Footer'
+import RestCard from './components/RestaurantCard/RestaurantCard'
 
 
 const  App = () => {
   return (
-      <Router />
+      // <Router />
+      <RestCard/>
   );
 }
 
--- src/components/Footer/Styled.js
+++ src/components/Footer/Styled.js
@@ -3,13 +3,14 @@ import { secColor } from '../../constants/colors'
 
 export const Footer = styled.div`
 display: flex;
-justify-content: space-around;
-width: 360px;
-height: 49px;
+justify-content: space-between;
+width: 320px;
+height: 40px;
 align-items: center;
 padding: 5px;
 box-sizing: border-box;
 border-top: 1px solid ${secColor};
 bottom: 0;
 position: fixed;
+margin: 1px auto;
  `;
