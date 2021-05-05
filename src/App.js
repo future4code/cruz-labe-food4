a90d628a9528b503458269f2b21dea9d985ad298
Amanda Lentez <amanda.a.lentz@gmail.com>
2021-05-05 18:06:10 -0300

    footer e restaurant card (components)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 
-import { Footer } from 'antd/lib/layout/layout';
 import React from 'react'
 import Router from './routes/router'
-import Foot from './components/Footer/Footer'
-import RestCard from './components/RestaurantCard/RestaurantCard'
 
 
 const  App = () => {
   return (
-      // <Router />
-      <RestCard/>
+      <Router />
   );
 }
 
