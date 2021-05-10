53c870e69faae50011b0a81059491f7df9e03d35
Fayra Miranda <miranda.fayra@gmail.com>
2021-05-10 10:25:49 -0300

    Adicionar informações requisição carrinho

--- src/components/AdressCard/AdressCard.js
+++ src/components/AdressCard/AdressCard.js
@@ -1,11 +1,17 @@
 import React from  'react'
 import {DivAdress, TitleAdress, EditTextAdress} from './StyledAdress'
+import { useHistory} from 'react-router-dom'
+import  useRequestData from '../../hooks/useRequestData'
 
 export const AdressCard=()=>{
+    const history =  useHistory()
+    const [profile,setProfile] = useRequestData(`/profile/address`,{})
+
     return(
         <DivAdress>
             <TitleAdress>Endereço Cadastro</TitleAdress>
-            <EditTextAdress>Rua Alessandra Vieira, 42 -Santana</EditTextAdress>
+            <EditTextAdress>{ profile.address && profile.address.street},{profile.address && profile.address.number} -
+            { profile.address && profile.address.neighbourhood} -{ profile.address && profile.address.city}</EditTextAdress>
         </DivAdress>
     )
 }
