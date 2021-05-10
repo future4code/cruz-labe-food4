d2aefe9e57732954fa19ceb6781ce019d17e084f
Fayra Miranda <miranda.fayra@gmail.com>
2021-05-10 11:42:13 -0300

    Finalizar estilização profile

--- src/pages/ProfilePage/EditProfile/EditRegister/StyledEditRegister.js
+++ src/pages/ProfilePage/EditProfile/EditRegister/StyledEditRegister.js
@@ -37,3 +37,24 @@ export const LogoImage = styled.img `
   max-width: 350px;
   margin: 10px;
 `
+export const DivProfile = styled.div `
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+`
+
+export const HederText = styled.p `
+    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, 
+    Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size:16px;
+    font-weight:500;
+    padding-top:12px;
+`
+
+export const BackButton = styled.img`
+  width: 23px;
+  height: 24px;
+  margin: 10px 321px 0 16px;
+  object-fit: contain;
+`
